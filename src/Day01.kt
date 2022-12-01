da90c9f7d7814502345cf2234f32231bc027d84a
Adrian Sanchez <aamiel.sanchez@gmail.com>
2022-12-01 09:00:50 +0100

    Add solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
+private class Elf(
+    val calories: MutableList<Int> = mutableListOf(),
+) {
+    val totalCalories: Int
+        get() = calories.sum()
+}
+
+typealias Input = List<String>
+
+private fun Input.toElfList(): List<Elf> =
+    fold(emptyList()) { acc, calorie ->
+        when {
+            calorie.isEmpty() -> acc + listOf(Elf())
+            else -> {
+                val lastElf = if (acc.isEmpty()) Elf() else acc.last()
+                acc.dropLast(1) + listOf(lastElf.also { it.calories.add(calorie.toInt()) })
+            }
+        }
+    }
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    // Get the total calories from the elf carrying the most calories
+    fun part1(input: List<String>): Int =
+        input.toElfList()
+            .maxBy { it.totalCalories }
+            .totalCalories
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // Get the total calories from the top 3 elves carrying the most calories
+    fun part2(input: List<String>): Int =
+        input.toElfList()
+            .sortedByDescending { it.totalCalories }
+            .take(3)
+            .sumOf { it.totalCalories }
 
     val input = readInput("Day01")
     println(part1(input))
