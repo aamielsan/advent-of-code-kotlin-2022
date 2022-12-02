bc9d283b9af6fecaeba8239f1c855a9ef5d03eb9
Adrian Sanchez <aamiel.sanchez@gmail.com>
2022-12-02 19:39:23 +0100

    Refactor solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,38 +1,34 @@
 private class Elf(
-    val calories: MutableList<Int> = mutableListOf(),
+    val calories: List<Int>,
 ) {
     val totalCalories: Int
         get() = calories.sum()
-}
 
-typealias Input = List<String>
-
-private fun Input.toElfList(): List<Elf> =
-    fold(emptyList()) { acc, calorie ->
-        when {
-            calorie.isEmpty() -> acc + listOf(Elf())
-            else -> {
-                val lastElf = if (acc.isEmpty()) Elf() else acc.last()
-                acc.dropLast(1) + listOf(lastElf.also { it.calories.add(calorie.toInt()) })
-            }
-        }
+    companion object {
+        fun fromCalories(string: String): Elf =
+            Elf(calories = string.lines().map(String::toInt))
     }
+}
 
 fun main() {
     // Get the total calories from the elf carrying the most calories
-    fun part1(input: List<String>): Int =
-        input.toElfList()
+    fun part1(input: String): Int =
+        input
+            .split("\n\n")
+            .map { Elf.fromCalories(it) }
             .maxBy { it.totalCalories }
             .totalCalories
 
     // Get the total calories from the top 3 elves carrying the most calories
-    fun part2(input: List<String>): Int =
-        input.toElfList()
+    fun part2(input: String): Int =
+        input
+            .split("\n\n")
+            .map { Elf.fromCalories(it) }
             .sortedByDescending { it.totalCalories }
             .take(3)
             .sumOf { it.totalCalories }
 
-    val input = readInput("Day01")
+    val input = readInputToText("Day01")
     println(part1(input))
     println(part2(input))
 }
